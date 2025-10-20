a674558728e68bd817fce0590af03a9faf28b402
KartikVerma39 <kartikverma1074@gmail.com>
2025-10-20 17:29:22 +0530

    objects_further

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -38,4 +38,39 @@ const obj4 = {...obj1 , ...obj2}
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+course.courseInstructor
+
+const {courseInstructor : instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+// const navbar = ({company}) =>{
+
+// }
+
+// navbar(company = "hitesh")
+
+
+// {
+//     name: "hitesh",
+//     coursename: "js in hindi",
+//     price: free
+// }
+
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
