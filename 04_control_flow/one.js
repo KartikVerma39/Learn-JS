8d8bca53130f6399ff5274bba451f255587bd10b
KartikVerma39 <kartikverma1074@gmail.com>
2025-10-22 00:06:15 +0530

    ifElse

---
+++ 04_control_flow/one.js
@@ -0,0 +1,55 @@
+// if
+
+const isUserloggedIn = true
+const temperature = 41
+
+// if (temperature === 40){
+//     console.log("executed");
+// }
+// else{
+//     console.log("temperature is greater than 50");
+// }
+
+
+// console.log("Execute");
+
+
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`)
+// }
+
+// console.log(`User Power: ${power}`);
+
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("test2");
+
+
+// if (balance > 500) {
+//     console.log("less than");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else{
+//     console.log("less than 1200");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard && 2 == 2){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("User logged in")
+}
+
+
